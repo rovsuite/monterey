7cbe575e7e01c48f42f27537ba3d785a58dd798b
Chris Konstad <chriskon149@gmail.com>
2013-08-21 16:59:02 -0700

    Remove voltage warning from ROVSettings window
    The voltage warning was kinda useless, so it was removed.

--- Monterey/rovsettings.cpp
+++ Monterey/rovsettings.cpp
@@ -39,7 +39,6 @@ ROVSettings::ROVSettings(QWidget *parent) :
     ui->leNamesSensor1->setText(p->controller->rov->sensorOther1->getName());
 
     ui->sbDepth->setValue(p->controller->rov->sensorDepth->getThreshold());
-    ui->sbVoltage->setValue(p->controller->rov->sensorVoltage->getThreshold());
 
     ui->comboMotorLayout->setCurrentIndex((int)p->controller->getMotorLayout());
     ui->cbBilinearEnabled->setChecked(p->controller->getBilinearEnabled());
@@ -82,7 +81,6 @@ void ROVSettings::on_pbSave_clicked()
     p->controller->rov->sensorOther0->setName(ui->leNamesSensor0->text());
     p->controller->rov->sensorOther1->setName(ui->leNamesSensor1->text());
     p->controller->rov->sensorDepth->setThreshold(ui->sbDepth->value());
-    p->controller->rov->sensorVoltage->setThreshold(ui->sbVoltage->value());
     p->controller->setMotorLayout(ui->comboMotorLayout->currentIndex());
     p->controller->setBilinearRatio(ui->sbBilinearRatio->value());
     p->controller->setBilinearEnabled(ui->cbBilinearEnabled->isChecked());
